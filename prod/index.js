2944a21f57d58b6dc82aacf6fb99bde6aadb3fad
TeamNitrous <88808337+TeamNitrous@users.noreply.github.com>
2022-05-01 20:48:02 -0700

    Add files via upload

---
+++ prod/index.js
@@ -0,0 +1,2 @@
+let contentdiv = document.getElementById('content');
+contentdiv.innerHTML += '<iframe width="100%" height="100%" src="http://pp.zombienw.com/prod/latest.html" frameborder="0"></iframe>';
