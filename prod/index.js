1271e1587d1738f3fcb9d0fe5c0c05c7a235b5cf
TeamNitrous <88808337+TeamNitrous@users.noreply.github.com>
2022-05-01 20:50:44 -0700

    Update index.js

--- prod/index.js
+++ prod/index.js
@@ -1,2 +1,2 @@
 let contentdiv = document.getElementById('content');
-contentdiv.innerHTML += '<iframe width="100%" height="100%" src="http://pp.zombienw.com/prod/latest.html" frameborder="0"></iframe>';
+contentdiv.innerHTML += '<iframe width="100%" height="100%" src="https://rawcdn.githack.com/TeamNitrous/projectplatinum/a48b708adae30560b370d2c9791e61befa26c1cb/prod/latest.html" frameborder="0"></iframe>';
